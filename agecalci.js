7a23d138b0f008715d1ae6740435dd1946725de9
IndiraErugurala <167762229+IndiraErugurala@users.noreply.github.com>
2024-05-27 18:01:45 +0530

    Add files via upload

---
+++ agecalci.js
@@ -0,0 +1,18 @@
+function calculateAge() {
+    const dob = document.getElementById("dob").value;
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+
+    const today = new Date();
+    const birthDate = new Date(year, month - 1, dob);
+    
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    
+    document.getElementById("result").innerText = `Your age is: ${age}`;
+}
+
